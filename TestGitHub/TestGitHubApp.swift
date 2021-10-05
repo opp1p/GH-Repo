9f4bcc765832b2a0a83e3a27001111328731102d
Filippo <f.monti93@gmail.com>
2021-10-05 19:11:28 +0200

    Initial Commit

---
+++ TestGitHub/TestGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGitHubApp.swift
+//  TestGitHub
+//
+//  Created by Filippo Monti on 05/10/21.
+//
+
+import SwiftUI
+
+@main
+struct TestGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
